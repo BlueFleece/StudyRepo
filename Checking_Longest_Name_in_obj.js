1b713abed54e4bc0ef9d62355d1d7cb4186a5c94
BlueFleece <111695940+BlueFleece@users.noreply.github.com>
2022-08-23 11:31:30 -0400

    Update Checking_Longest_Name_in_obj.js

--- Checking_Longest_Name_in_obj.js
+++ Checking_Longest_Name_in_obj.js
@@ -1,3 +1,18 @@
+// Get Longest Name
+// Write a function, getLongestName, that accepts an object as an argument. The object represents a family tree. Return the longest name in the family.
+
+// let family = {
+//   'Beverly Marquez': {
+//     'Nina Rhone': {
+//       'William Rhodes': null,
+//       'Paul Nell': null,
+//       'Sir Paddington the Fourth, of the county Wilstonshire': null
+//     }
+//   }
+// };
+
+// getLongestName(family); // => 'Sir Paddington the Fourth, of the county Wilstonshire'
+
 function getLongestName(obj){
   let longestName = '';
   for (let key in obj){
