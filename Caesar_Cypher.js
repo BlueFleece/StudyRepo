482c3a6abab2775196c2a052c220df249cee5c3a
BlueFleece <111695940+BlueFleece@users.noreply.github.com>
2022-08-23 11:06:20 -0400

    Update Caesar_Cypher.js

--- Caesar_Cypher.js
+++ Caesar_Cypher.js
@@ -1,4 +1,15 @@
-// YOUR CODE BELOW
+// Caesar Cypher
+// A Caesar Cypher is a classic encryption technique favored by the ancient Romans.
+
+// Define a function, caesarCypher, that accepts two arguments:
+
+// secret (string) - the string we want to encrypt
+// shift (num) - the number we want to rotate each letter in the string by
+// caesarCypher should return an encrypted version of the string shifted by the number of letters supplied. For example, the string 'abc' shifted by 2 should return 'cde'. Spaces between words should not be rotated. Note that any shifts that exceed the end of the alphabet should wrap around. For example, the string 'xyz' shifted by 2 should return 'zab', the string 'hello' shifted by 13 should return 'urryb'.
+
+// You can assume the shift number will be a positive integer. Additionally, you may assume that the secret string will be entirely lower case
+
+// caesarCypher('hello world', 13); // => 'uryyb jbeyq'
 
 function caesarCypher(secret, shift) {
     var alphabet = "abcdefghijklmnopqrstuvwxyz";
